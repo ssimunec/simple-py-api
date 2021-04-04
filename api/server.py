38303e7772ee405dda8ad5061e9303c413149960
stanislav.simunec <stanislavsimunec@gmail.com>
2021-04-04 19:28:11 +0200

    Initial commit

---
+++ api/server.py
@@ -0,0 +1,19 @@
+from flask_cors import CORS
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+CORS(app)
+debug = False
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return jsonify({
+        'version': '0.0.1b',
+        'response': 'hello world from API !',
+        'status': 'sucess'
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8004)
