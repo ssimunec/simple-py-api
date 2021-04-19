c88f3f79042a5c714c864d7aeb119c65b81c923e
stanislav.simunec <stanislavsimunec@gmail.com>
2021-04-19 22:37:54 +0200

    insert i select

--- api/server.py
+++ api/server.py
@@ -1,8 +1,16 @@
 from flask_cors import CORS
 from flask import Flask, jsonify
+import pymongo
 
 app = Flask(__name__)
 CORS(app)
+
+konekcija_na_bazu = pymongo.MongoClient("mongodb://10.0.0.21:27017",
+                                        username='root',
+                                        password='123')
+
+baza = konekcija_na_bazu["mydatabase"]
+kolekcija = baza["customers"]
 
 
 @app.route('/hello', methods=['GET', 'POST'])
@@ -10,6 +18,37 @@ def hello():
     return jsonify({
         'version': '0.0.1b',
         'response': 'hello world from API !',
+        'status': 'sucess'
+    }), 200
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+
+    moj_rekors = {"name": "John", "address": "Highway 37"}
+
+    x = kolekcija.insert_one(moj_rekors)
+
+    return jsonify({
+        'version': '0.0.1b',
+        'response': 'record added',
+        'status': 'sucess'
+    }), 200
+
+
+@app.route('/select', methods=['GET'])
+def select():
+
+    moji_rekordi = kolekcija.find()
+
+    lista = []
+
+    for x in moji_rekordi:
+        lista.append({"name": x["name"], "address": x["address"]})
+
+    return jsonify({
+        'version': '0.0.1b',
+        'response': lista,
         'status': 'sucess'
     }), 200
 
